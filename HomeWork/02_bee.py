d697c494e2b5dbc56beb4321d12eac6b55de857c
kkallen <168973391+kkallen1@users.noreply.github.com>
2025-03-24 11:32:23 +0800

    Update 02_bee.py

--- HomeWork/02_bee.py
+++ HomeWork/02_bee.py
@@ -3,35 +3,25 @@
 
 m, n, p = map(int, input().split())
 List = []
-for _ in rnage(m):
-  L = list(input().split())
-  List.append(L)
-k = list(int, input().split())
-
-# starting point
-a, b = m-1, 0
+for _ in range(m):
+  List.append(input())
+move = [int(x) for x in input().split()]
 
 # move rules: 0 1 2 3 4 5
 r = (-1, 0, 1, 1, 0, -1) # row
 c = (0, 1, 1, 0, -1, -1) # column
 
-# move
-def f(i):
-  if (a+r[i]) < m:
-    a += r[i]
-  if (b+c[i]) < n:
-    b += c[i]
+# starting point
+a, b = m-1, 0
 
-for i in k:
-  if i==0:
-    f(i)
-  elif i==1:
-    f(i)
-  elif i==2:
-    f(i)
-  elif i==3:
-    f(i)
-  elif i==4:
-    f(i)
-  elif i==5:
-    f(i)
+ans = []
+for i in move:
+  tmp_a, tmp_b = a+r[i], b+c[i]
+
+  if 0 <= tmp_a < m and 0 <= tmp_b < n:
+    a, b = tmp_a , tmp_b
+
+  ans.append(List[a][b])
+
+print(*ans, sep="")
+print(len(set(ans)))
