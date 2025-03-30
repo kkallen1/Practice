1e4f9c81db5cdcd10723d105cea86d19c8c88cef
kkallen <168973391+kkallen1@users.noreply.github.com>
2025-03-30 10:45:19 +0800

    Update K.py

--- 111/Program/K.py
+++ 111/Program/K.py
@@ -20,3 +20,29 @@ while n != 0:
     print(f"{n} : {f(n)}")
 
     n = int(input())
+
+
+# WA code
+
+def f(n):
+    ans = []
+
+    if n%2 == 0:
+        ans.append(2)
+        while n%2 == 0:
+            n//=2
+
+    for i in range(3, n+1, 2):
+        if n%i == 0:
+            ans.append(i)
+            while n%i == 0:
+                n//=i
+    
+    return len(ans) if ans else 1
+        
+
+n = int(input())
+while n != 0:
+    print(f"{n} : {f(n)}")
+
+    n = int(input())
