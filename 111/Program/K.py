4c92a1bd661f9cee6b4ebc4fbf7a01d066de29da
kkallen <168973391+kkallen1@users.noreply.github.com>
2025-03-30 11:41:31 +0800

    Update K.py

--- 111/Program/K.py
+++ 111/Program/K.py
@@ -1,29 +1,4 @@
-# TLE code
-
-def f(n):
-    ans = []
-
-    i = 2
-    while n>1:
-        if n%i==0:
-            if i  not in ans:
-                ans.append(i)
-            n //= i
-        else:
-            i += 1
-    
-    return len(ans) if ans else 1
-        
-
-n = int(input())
-while n != 0:
-    print(f"{n} : {f(n)}")
-
-    n = int(input())
-
-
-# WA code
-
+# AC code
 def f(n):
     ans = []
 
@@ -37,6 +12,28 @@ def f(n):
             ans.append(i)
             while n%i == 0:
                 n//=i
+    
+    return len(ans)
+        
+
+n = int(input())
+while n != 0:
+    print(f"{n} : {f(n)}")
+
+    n = int(input())
+
+# TLE code
+def f(n):
+    ans = []
+
+    i = 2
+    while n>1:
+        if n%i==0:
+            if i  not in ans:
+                ans.append(i)
+            n //= i
+        else:
+            i += 1
     
     return len(ans) if ans else 1
         
