582e7956851fa7671f56f41eaf602b38bb46d99a
kkallen <168973391+kkallen1@users.noreply.github.com>
2025-03-31 15:00:10 +0800

    Create 1102J1.py

---
+++ 110/Program/1102J1.py
@@ -0,0 +1,28 @@
+# TLE code
+def f(n):
+    ans = []
+
+    if n%2 == 0:
+        ans.append(2)
+        while n%2 == 0:
+            n//=2
+
+    for i in range(3, n+1, 2):
+        if n%i == 0:
+            ans.append(i)
+            while n%i == 0:
+                n//=i
+    
+    if 4 in ans: return False
+    for i in range(2,6,1):
+        try:
+            ans.remove(i)
+        except:
+            continue
+    
+    return True if (len(ans)==0) else False
+
+n = int(input())
+L = [int(x) for x in input().split()]
+for i in L:
+    print(f(n))
