c9d4aab760ca0dade32f9e88b04cf9a506d935d2
kkallen <168973391+kkallen1@users.noreply.github.com>
2025-03-31 10:40:33 +0800

    Update 01.py

--- HomeWork/01.py
+++ HomeWork/01.py
@@ -4,8 +4,8 @@ https://zerojudge.tw/ShowProblem?problemid=m931
 
 m = int(input()) # 選擇要輸出第幾名的能力值
 n = int(input()) # 有幾個角色
-L = []
-L1 = []
+L = []           # 輸入的資料
+L1 = []          # 計算後的能力值
 for _ in range(n):
     a, b = map(int, input().split())
     L.append([a,b])
