385a4ad968a5668595e0a69b4897f1e7eb1cd5c4
kkallen <168973391+kkallen1@users.noreply.github.com>
2025-04-03 12:59:18 +0800

    Update 1102H.py

--- 110/Program/1102H.py
+++ 110/Program/1102H.py
@@ -14,6 +14,37 @@ for _ in range(n):
     # 轉為1的補數並轉回 十六進制
     print(hex(Sum^0xffff)[-4:])
 
+# AC code
+n = int(input())
+for _ in range(n):
+    L = [x for x in input().split()]
+    
+    Sum = 0
+    for i in range(0, 20, 2):
+        Sum += int( (L[i]+L[i+1]), 16)
+    
+    # 轉回 十六進制
+    Sum = hex(Sum)[2::]
+    
+    # 若長度>4  =>  超過 2 Byte  =>  進位的再加回來
+    if len(Sum)>4:
+        tmp = Sum[0:len(Sum)-4]
+        Sum = int(Sum[1::], 16) + int(tmp, 16)
+        Sum = hex(Sum)[2::]
+
+    Sum = bin( int(Sum, 16) )[2::] # 轉為二進制
+    # 若最前面為0，會自動刪除，所以要補0
+    if len(Sum) < 16:
+        Sum = "0"*(16-len(Sum)) + Sum
+    Sum = ["1" if x=="0" else "0" for x in Sum] # 取1的補數
+    Sum = "".join(str(x) for x in Sum) # 轉回字串
+    Sum = int(Sum, 2) # 轉為十進制
+    Sum = hex(Sum)[2::] # 轉回 十六進制
+    
+    if Sum != "0":
+        print(Sum)
+    else:
+        print("0000")
 
 # WA code
 n = int(input())
