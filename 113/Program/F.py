687ed6ba23663432b6b341be0f08104c7e20bf8b
kkallen <168973391+kkallen1@users.noreply.github.com>
2025-04-04 14:05:21 +0800

    Update F.py

--- 113/Program/F.py
+++ 113/Program/F.py
@@ -1,3 +1,24 @@
+m, n, n, p = map(int, input().split())
+
+List1 = []
+List2 = []
+for i in range(m):
+    L = [int(x) for x in input().split()]
+    List1.append(L)
+for i in range(n):
+    L = [int(x) for x in input().split()]
+    List2.append(L)
+
+ans = [[0]*p for _ in range(m)]
+for i in range(m):
+    for j in range(p):
+        for k in range(n):
+            ans[i][j] += List1[i][k]*List2[k][j]
+
+for i in ans:
+    print(*i)
+
+# 使用庫
 import numpy as np
 
 m,n,n,p = map(int, input().split())
