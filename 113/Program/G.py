77df57c99dd8e18fa970683cee3e7626c29c12df
kkallen <168973391+kkallen1@users.noreply.github.com>
2025-04-04 13:36:48 +0800

    Update G.py

--- 113/Program/G.py
+++ 113/Program/G.py
@@ -1,3 +1,15 @@
+m, n = map(int, input().split())
+List = [[0]*m for _ in range(n)]
+for i in range(m):
+    L = [int(x) for x in input().split()]
+
+    for j in range(n):
+        List[j][i] = L[j]
+
+for i in List:
+    print(*i)
+
+# 使用庫
 import numpy as np
 
 m,n = map(int, input().split())
