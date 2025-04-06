5a7679b8663182b6685c85ea5781f9cb98704c8d
kkallen <168973391+kkallen1@users.noreply.github.com>
2025-04-06 17:20:53 +0800

    Update L.py

--- 111/Program/L.py
+++ 111/Program/L.py
@@ -1,5 +1,14 @@
-# TLE code
+# AC code
+n = int(input())
 
+ans = 0
+for i in range(1, n+1, 1):
+    kk = bin(i)[2::]
+    ans += kk.count("1")
+
+print(ans)
+
+# TLE code
 kk = f""
 n = int(input())
 for i in range(1, n+1, 1):
